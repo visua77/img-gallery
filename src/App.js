1c7fb5427c9ed6a648780c4591443f8408c6b84c
Niclas Binnquist <niclasbinnquist@h-129-101.A148.priv.bahnhof.se>
2020-09-19 00:14:59 +0200

    tweaking styling and added responsive version

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ import {Search} from './components/Search'
 
 export const App = () => {
 
-/*   const[images, setImages] = useState([])
-
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => {
-        setImages(json.hits)
-      })
-  }, []) */
- 
-
-//console.log(images)
-
   return (
     <div className="main">
       <Search />
